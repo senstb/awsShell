46e5f7f5ad64c61195add7f59b6589b31cf0bdbb
Senst <senstb@acbc3292fb4f.ant.amazon.com>
2018-09-06 13:24:41 -0700

    Added index.html file creation

--- startup.sh
+++ startup.sh
@@ -5,5 +5,6 @@ sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo yum install -y httpd
+sudo touch ~/../../var/www/html/index.html
 sudo service httpd start
 sudo chkconfig httpd on
