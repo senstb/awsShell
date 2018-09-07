0dbd9951cd7e3164e161df199c8ff4a8be31bfae
Senst <senstb@acbc3292fb4f.ant.amazon.com>
2018-09-07 16:44:13 -0700

    Updated stop script to only grab running instances

--- stop_all_ec2.sh
+++ stop_all_ec2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export instanceID=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceID]' --output text)
+export instanceID=$(aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[InstanceId]' --output text)
 
 if [[ $instanceID = *"None"* ]]; then {
   echo "No instances are currently running"
