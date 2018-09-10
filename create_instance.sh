e4ba3c6e10986a019d78c01f3816d9e5bb8d82a9
Senst <senstb@acbc3292fb4f.ant.amazon.com>
2018-09-10 09:49:37 -0700

    Added error handling around JQ application dependencies

--- create_instance.sh
+++ create_instance.sh
@@ -3,6 +3,15 @@
 echo "Please enter name for instance: "
 read instanceName
 
+export jqVerify=$(command -v jq)
+
+if [ -z "$jqVerify" ]
+then
+  echo "Error in script: "
+  echo "JQ application required to run script. Please review documentation for installation at https://stedolan.github.io/jq/"
+  exit 1
+fi
+
 export instAmi=$(aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
 
 export keyName=$(aws ec2 describe-key-pairs | jq -r '.KeyPairs | sort_by(.CreationDate) | last(.[]).KeyName')
